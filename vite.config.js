e228fe0a3bb961e65eb950cf2da288025c309d9b
Boburjon <shadow88bob@gmail.com>
2025-07-29 07:48:13 +0500

    add tailwind

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,11 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/',
   plugins: [vue()],
+  server: {
+    host: '0.0.0.0',
+    port: 8080,
+    open: true,
+  },
 })
